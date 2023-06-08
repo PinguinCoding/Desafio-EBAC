9f71a9cfa896768d3a760835b08b8c02c3f4f862
Guilherme <112185084+PinguinCoding@users.noreply.github.com>
2023-06-08 12:00:31 -0300

    Create Test-Module.py

---
+++ Test-Module.py
@@ -0,0 +1,19 @@
+y = 1700
+time_machine(y)
+# Deve retornar 12/9/1700
+
+y = 1786
+time_machine(y)
+# Deve retornar 13/9/1786
+
+y = 1918
+time_machine(y)
+# Deve retornar 26/9/1918
+
+y = 2017
+time_machine(y)
+# Deve retornar 13/9/2017
+
+y = 2700
+time_machine(y)
+# Deve retornar 13/9/2700
