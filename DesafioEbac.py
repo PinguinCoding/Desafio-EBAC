b4717049ae1b3d574d5a7d36bc05fed6ab99a714
Guilherme <112185084+PinguinCoding@users.noreply.github.com>
2023-06-08 11:56:56 -0300

    Update DesafioEbac.py

--- DesafioEbac.py
+++ DesafioEbac.py
@@ -1,36 +1,87 @@
-from datetime import datetime, timedelta
+# Cria duas listas representando os calendários para anos normais e bissextos
+normal_year = [[day for day in range(0, 31)],
+              [day for day in range(0, 28)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)]]
+
+leap_year =  [[day for day in range(0, 31)],
+              [day for day in range(0, 29)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)],
+              [day for day in range(0, 30)],
+              [day for day in range(0, 31)]]
 
 
-def maquina_do_tempo(ano_destino):
-    try:
-        if ano_destino == 1918:
-            data = datetime(ano_destino, 1, 1) + timedelta(days=268)
-            return data.strftime("%d/%m/%Y")
+def time_machine(year_destiny):
+  # Lida com caso do ano de transição de calendário 1918
+  if year_destiny == 1918:
+    count = 0
+    # Exclui o 1º mês e começa a partir de Fevereiro
+    for month in range(1, len(normal_year)+1):
+      if month == 1:
+        # Percorre o mês de fevereiro a partir do dia 14
+        for day in range(14, 29):
+          count += 1
+      # Percorre os demais meses normalmente
+      for day in range(0, len(normal_year[month])):
+        count += 1
+        # Quando chega na data desejada, retorna a data formatada
+        if count == 253:
+          return str(normal_year[month][day+1]) + '/' + str(month+1) + '/' + str(year_destiny)
+  
+  # Lida com o caso de anos no calendário juliano
+  elif 1700 <= year_destiny <= 1917:
+    count = 0
+    # Identifica se o ano é bissexto
+    if year_destiny % 4 == 0:
+      # Percorre o ano e conta os dias
+      for month in range(0, len(leap_year)):
+        for day in range(0, len(leap_year[month])):
+          count += 1
+          # Retorna o 256º dia do dado ano
+          if count == 256:
+            return str(leap_year[month][day+1]) + '/' + str(month+1) + '/' + str(year_destiny)
+    # Ano que NÃO é bissexto
+    else:
+      for month in range(0, len(normal_year)):
+        for day in range(0, len(normal_year[month])):
+          count += 1
+          if count == 256:
+            return str(normal_year[month][day+1]) + '/' + str(month+1) + '/' + str(year_destiny)
 
-        elif 1917 >= ano_destino >= 1700:
-            if ano_destino % 4 == 0:
-                data = datetime(ano_destino, 1, 1) + timedelta(days=254)
-                return data.strftime("%d/%m/%Y")
-
-            else:
-                data = datetime(ano_destino, 1, 1) + timedelta(days=255)
-                return data.strftime("%d/%m/%Y")
-
-        elif 2700 >= ano_destino >= 1919:
-            data = datetime(ano_destino, 1, 1) + timedelta(days=255)
-            return data.strftime("%d/%m/%Y")
-
-        else:
-            return print('Não é possível viajar para esse ano!')
-
-    except ValueError:
-        return print(f'{ano_destino} não é um valor válido para a viagem!')
-
-    except TypeError:
-        return print(f'{ano_destino} não é um tipo de entrada válido para a viagem!')
-
-    except Exception as exc:
-        raise exc
-
-y = int(input('Input: '))
-print(f'Output: {maquina_do_tempo(y)}')
+  # Lida com o caso de anos no calendário gregoriano
+  elif 1919 <= year_destiny <= 2700:
+    count = 0
+    # Identifica se o ano é bissexto
+    if year_destiny % 4 == 0 and year_destiny % 100 !=0 or year_destiny % 400 == 0:
+      # Percorre o ano e conta os dias
+      for month in range(0, len(leap_year)):
+        for day in range(0, len(leap_year[month])):
+          count += 1
+          # Retorna o 256º dia do dado ano
+          if count == 256:
+            return str(leap_year[month][day+1]) + '/' + str(month+1) + '/' + str(year_destiny)
+    # Ano que NÃO é bissexto
+    else:
+      for month in range(0, len(normal_year)):
+        for day in range(0, len(normal_year[month])):
+          count += 1
+          if count == 256:
+            return str(normal_year[month][day+1]) + '/' + str(month+1) + '/' + str(year_destiny)
+  
+  else:
+    return print('Cannot travel past year 2700')
